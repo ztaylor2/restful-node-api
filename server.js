efe18b3976fff0af0e20f4df2258a8aa1d2cb6b4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-29 15:10:16 -0700

    route for get bears returning 404 error.

--- server.js
+++ server.js
@@ -39,10 +39,8 @@ router.get('/', function(req, res) {
 // on routes that end in /bears
 // ----------------------------------------------------
 router.route('/bears')
-
     // create a bear (accessed at POST http://localhost:8080/api/bears)
     .post(function(req, res) {
-
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
 
@@ -54,8 +52,30 @@ router.route('/bears')
             res.json({ message: 'Bear created!' });
         });
 
+    })
+
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+
+        });
     });
 
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
