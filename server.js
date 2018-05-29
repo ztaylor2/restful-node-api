307d82fed668b31854f0f2f657f268d0b36dbb35
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-29 14:41:44 -0700

    changed database uri to localhost.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+mongoose.connect('mongodb://127.0.0.1:27017/'); // connect to our database
 
 
 // ROUTES FOR OUR API
@@ -35,6 +35,27 @@ router.get('/', function(req, res) {
 });
 
 // more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
