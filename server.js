3abea69bde7b8616a46654bb8527693ec0fab093
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-29 15:33:57 -0700

    created a delete route to delete a specific bear by id.

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ router.get('/', function(req, res) {
 // on routes that end in /bears
 // ----------------------------------------------------
 router.route('/bears')
+
     // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
         Bear.find(function(err, bears) {
@@ -50,6 +51,7 @@ router.route('/bears')
 
         });
     })
+
     // create a bear (accessed at POST http://localhost:8080/api/bears)
     .post(function(req, res) {
         var bear = new Bear();      // create a new instance of the Bear model
@@ -75,6 +77,39 @@ router.route('/bears/:bear_id')
             if (err)
                 res.send(err);
             res.json(bear);
+        });
+    })
+
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+
+    .delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
         });
     });
 
