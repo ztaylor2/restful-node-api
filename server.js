711c2b1c16e69dfb5b9981a8bc71f98e96ee4f5d
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-31 15:31:28 -0700

    changed all funcitons to arrow functions.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ router.use(function(req, res, next) {
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
@@ -41,8 +41,8 @@ router.get('/', function(req, res) {
 router.route('/bears')
 
     // get all the bears (accessed at GET http://localhost:8080/api/bears)
-    .get(function(req, res) {
-        Bear.find(function(err, bears) {
+    .get((req, res) => {
+        Bear.find((err, bears) => {
 
             if (err)
                 res.send(err);
@@ -53,12 +53,12 @@ router.route('/bears')
     })
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
+    .post((req, res) => {
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
 
         // save the bear and check for errors
-        bear.save(function(err) {
+        bear.save((err) => {
             if (err)
                 res.send(err);
 
@@ -72,8 +72,8 @@ router.route('/bears')
 router.route('/bears/:bear_id')
 
     // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    .get(function(req, res) {
-        Bear.findById(req.params.bear_id, function(err, bear) {
+    .get((req, res) => {
+        Bear.findById(req.params.bear_id, (err, bear) => {
             if (err)
                 res.send(err);
             res.json(bear);
@@ -81,10 +81,10 @@ router.route('/bears/:bear_id')
     })
 
     // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-    .put(function(req, res) {
+    .put((req, res) => {
 
         // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function(err, bear) {
+        Bear.findById(req.params.bear_id, (err, bear) => {
 
             if (err)
                 res.send(err);
@@ -92,7 +92,7 @@ router.route('/bears/:bear_id')
             bear.name = req.body.name;  // update the bears info
 
             // save the bear
-            bear.save(function(err) {
+            bear.save((err) => {
                 if (err)
                     res.send(err);
 
@@ -102,10 +102,10 @@ router.route('/bears/:bear_id')
         });
     })
 
-    .delete(function(req, res) {
+    .delete((req, res) => {
         Bear.remove({
             _id: req.params.bear_id
-        }, function(err, bear) {
+        }, (err, bear) => {
             if (err)
                 res.send(err);
 
